617ccf35cdda9a5673d12f7d7beafe96705008f2
varunpreet-code <cp0452472@gmail.com>
2020-08-08 11:26:29 +0800

    Add story to profile

--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -3,6 +3,7 @@ from django.views.generic.edit import CreateView
 from django.views import generic
 from .models import CustomUser
 from .forms import CustomUserCreationForm
+from news.models import NewsStory
 
 
 class CreateAccountView(CreateView):
@@ -15,6 +16,18 @@ class UserProfileView(generic.DetailView):
     template_name ='users/Profile.html'
     context_object_name = 'UserDetails'
 
+# def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['latest_stories'] = NewsStory.objects.all().order_by('-pub_date')[:4]
+#         context['all_stories'] = NewsStory.objects.all().order_by('-pub_date')
+#         return context
 
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['stories'] = NewsStory.objects.filter(author=self.kwargs['pk'])
+        print(context)
+        return context
 
 # Create your views here.
