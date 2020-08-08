036bcafb71ab7776b1e03fe5f14a548ed33eb6e6
varunpreet-code <cp0452472@gmail.com>
2020-08-08 10:26:56 +0800

    Profile added

--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -4,9 +4,17 @@ from django.views import generic
 from .models import CustomUser
 from .forms import CustomUserCreationForm
 
+
 class CreateAccountView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'users/createAccount.html'
+
+class UserProfileView(generic.DetailView):
+    model = CustomUser
+    template_name ='users/Profile.html'
+    context_object_name = 'UserDetails'
+
+
 
 # Create your views here.
